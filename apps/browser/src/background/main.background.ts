b15325763cad82c6cb4d1044771069671a6487df
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-09-20 11:46:00 -0400

    Remove Storage Reseed FF (#11156)

--- apps/browser/src/background/main.background.ts
+++ apps/browser/src/background/main.background.ts
@@ -1481,14 +1481,7 @@ export default class MainBackground {
     });
 
     if (needStorageReseed) {
-      await this.reseedStorage(
-        await firstValueFrom(
-          this.configService.userCachedFeatureFlag$(
-            FeatureFlag.StorageReseedRefactor,
-            userBeingLoggedOut,
-          ),
-        ),
-      );
+      await this.reseedStorage();
     }
 
     if (BrowserApi.isManifestVersion(3)) {
@@ -1543,7 +1536,7 @@ export default class MainBackground {
     await SafariApp.sendMessageToApp("showPopover", null, true);
   }
 
-  async reseedStorage(doFillBuffer: boolean) {
+  async reseedStorage() {
     if (
       !this.platformUtilsService.isChrome() &&
       !this.platformUtilsService.isVivaldi() &&
@@ -1552,11 +1545,7 @@ export default class MainBackground {
       return;
     }
 
-    if (doFillBuffer) {
-      await this.storageService.fillBuffer();
-    } else {
-      await this.storageService.reseed();
-    }
+    await this.storageService.fillBuffer();
   }
 
   async clearClipboard(clipboardValue: string, clearMs: number) {
