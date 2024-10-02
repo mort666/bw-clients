e1189e06c81f028511ed946894f6cec12b547629
Bernd Schoolmann <mail@quexten.com>
2024-10-02 16:19:46 +0200

    Add documentation to electronsshkeygeneratorservice generate function

--- apps/desktop/src/platform/services/electron-sshkey-generator.service.ts
+++ apps/desktop/src/platform/services/electron-sshkey-generator.service.ts
@@ -4,6 +4,13 @@ import { SshKeyData } from "@bitwarden/common/vault/models/data/ssh-key.data";
 import { SshKeyNativeGeneratorAbstraction } from "../../../../../libs/tools/generator/core/src/abstractions/sshkey-native-generator.abstraction";
 
 export class ElectronSshKeyGeneratorService implements SshKeyNativeGeneratorAbstraction {
+  /**
+   * Generate a new SSH key pair. This is done via IPC to the main process. The ssh key generation happens in the desktop_native module because it is implemented in Rust.
+   * This can be later replaced by an SDK-wasm implementation.
+   * @param keyAlgorithm The algorithm to use for the key generation. Currently only "rsa" and "ed25519" are supported.
+   * @param keyLength The length of the key to generate in bits. Only used for RSA keys.
+   * @returns The generated SSH key pair.
+   */
   async generate(keyAlgorithm: "rsa" | "ed25519", keyLength?: number): Promise<SshKeyData> {
     let algorithmNameWithBits = keyAlgorithm;
     if (keyAlgorithm === "rsa") {
