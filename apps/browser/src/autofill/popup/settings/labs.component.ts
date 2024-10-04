c380f1a8415c5a5bf28cae8963fb0e2df90fae2f
Jonathan Prusik <jprusik@classynemesis.com>
2024-10-04 16:51:54 -0400

    add experimental settings page and routing

---
+++ apps/browser/src/autofill/popup/settings/labs.component.ts
@@ -0,0 +1,79 @@
+import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { firstValueFrom } from "rxjs";
+
+import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { LabsSettingsServiceAbstraction } from "@bitwarden/common/autofill/services/labs-settings.service";
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
+import {
+  CardComponent,
+  CheckboxModule,
+  FormFieldModule,
+  ItemModule,
+  SectionComponent,
+  SectionHeaderComponent,
+  TypographyModule,
+} from "@bitwarden/components";
+
+import { PopOutComponent } from "../../../platform/popup/components/pop-out.component";
+import { PopupHeaderComponent } from "../../../platform/popup/layout/popup-header.component";
+import { PopupPageComponent } from "../../../platform/popup/layout/popup-page.component";
+
+@Component({
+  templateUrl: "labs.component.html",
+  standalone: true,
+  imports: [
+    CardComponent,
+    CheckboxModule,
+    CommonModule,
+    FormFieldModule,
+    FormsModule,
+    ItemModule,
+    JslibModule,
+    PopOutComponent,
+    PopupHeaderComponent,
+    PopupPageComponent,
+    RouterModule,
+    SectionComponent,
+    SectionHeaderComponent,
+    TypographyModule,
+  ],
+})
+export class LabsComponent implements OnInit {
+  /*
+   * Default values set here are used in component state operations
+   * until corresponding stored settings have loaded on init.
+   */
+  protected labsSettingsEnabled$: boolean = false;
+  protected improvedFieldQualificationForInlineMenuEnabled: boolean = false;
+  protected additionalInlineMenuCipherTypesEnabled: boolean = false;
+
+  constructor(
+    private i18nService: I18nService,
+    private labsSettingsService: LabsSettingsServiceAbstraction,
+  ) {}
+
+  async ngOnInit() {
+    this.improvedFieldQualificationForInlineMenuEnabled = await firstValueFrom(
+      this.labsSettingsService.improvedFieldQualificationForInlineMenuEnabled$,
+    );
+
+    this.additionalInlineMenuCipherTypesEnabled = await firstValueFrom(
+      this.labsSettingsService.additionalInlineMenuCipherTypesEnabled$,
+    );
+  }
+
+  async updateImprovedFieldQualificationForInlineMenuEnabled() {
+    await this.labsSettingsService.setImprovedFieldQualificationForInlineMenuEnabled(
+      this.improvedFieldQualificationForInlineMenuEnabled,
+    );
+  }
+
+  async updateAdditionalInlineMenuCipherTypesEnabled() {
+    await this.labsSettingsService.setAdditionalInlineMenuCipherTypesEnabled(
+      this.additionalInlineMenuCipherTypesEnabled,
+    );
+  }
+}
