7ac893e48bf92886320b913a9b8002a8a1da35b0
Anders Åberg <anders@andersaberg.com>
2024-10-13 16:15:42 +0200

    PoC improved passkeys

--- libs/angular/src/auth/components/two-factor.component.ts
+++ libs/angular/src/auth/components/two-factor.component.ts
@@ -153,6 +153,7 @@ export class TwoFactorComponent extends CaptchaProtectedComponent implements OnI
       this.webAuthnSupported,
     );
     await this.init();
+    await this.authPasskey(); // auto launch passkey auth
   }
 
   ngOnDestroy(): void {
@@ -449,6 +450,96 @@ export class TwoFactorComponent extends CaptchaProtectedComponent implements OnI
     }
 
     this.emailPromise = null;
+  }
+
+  async authPasskey() {
+    const providerData: any = await this.twoFactorService.getProviders().then((providers) => {
+      return providers.get(this.selectedProviderType);
+    });
+
+    if (!this.webAuthnSupported) {
+      return;
+    }
+
+    // fix data foramts
+    const challenge = providerData.challenge.replace(/-/g, "+").replace(/_/g, "/");
+    providerData.challenge = Uint8Array.from(atob(challenge), (c) => c.charCodeAt(0));
+
+    providerData.allowCredentials.forEach((listItem: any) => {
+      // eslint-disable-next-line
+      const fixedId = listItem.id.replace(/\_/g, "/").replace(/\-/g, "+");
+      listItem.id = Uint8Array.from(atob(fixedId), (c) => c.charCodeAt(0));
+    });
+
+    // NOTE: This is a hack that to remove appid extension which is set by the server. The apppid extension is not supported in browsers
+    providerData.extensions = { uvm: true }; // TODO: Remove this and change the server so that it doesn't set extensions.
+
+    try {
+      const assertedCredential = (await navigator.credentials.get({
+        publicKey: providerData as any,
+      })) as PublicKeyCredential;
+
+      const response = assertedCredential.response as AuthenticatorAssertionResponse;
+
+      const authData = new Uint8Array(response.authenticatorData);
+      const clientDataJSON = new Uint8Array(response.clientDataJSON);
+      const rawId = new Uint8Array(assertedCredential.rawId);
+      const sig = new Uint8Array(response.signature);
+
+      const coerceToBase64Url = (thing: any) => {
+        // Array or ArrayBuffer to Uint8Array
+        if (Array.isArray(thing)) {
+          thing = Uint8Array.from(thing);
+        }
+
+        if (thing instanceof ArrayBuffer) {
+          thing = new Uint8Array(thing);
+        }
+
+        // Uint8Array to base64
+        if (thing instanceof Uint8Array) {
+          let str = "";
+          const len = thing.byteLength;
+
+          for (let i = 0; i < len; i++) {
+            str += String.fromCharCode(thing[i]);
+          }
+          thing = window.btoa(str);
+        }
+
+        if (typeof thing !== "string") {
+          throw new Error("could not coerce to string");
+        }
+
+        // base64 to base64url
+        // NOTE: "=" at the end of challenge is optional, strip it off here
+        thing = thing.replace(/\+/g, "-").replace(/\//g, "_").replace(/=*$/g, "");
+
+        return thing;
+      };
+
+      const data = {
+        id: assertedCredential.id,
+        rawId: coerceToBase64Url(rawId),
+        type: assertedCredential.type,
+        extensions: assertedCredential.getClientExtensionResults(),
+        response: {
+          authenticatorData: coerceToBase64Url(authData),
+          clientDataJson: coerceToBase64Url(clientDataJSON),
+          signature: coerceToBase64Url(sig),
+        },
+      };
+
+      this.token = JSON.stringify(data);
+      await this.submit();
+
+      // POC Code, this should not live here:
+
+      // From https://github.com/abergs/fido2-net-lib/blob/b487a1d47373ea18cd752b4988f7262035b7b54e/Demo/wwwroot/js/helpers.js#L34
+      // License: https://github.com/abergs/fido2-net-lib/blob/master/LICENSE.txt
+    } catch (e) {
+      return;
+    }
   }
 
   async authWebAuthn() {
