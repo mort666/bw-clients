b5cdf4d6f7638899b5232e2ef21be633852a8c38
Alec Rippberger <alec@livefront.com>
2024-10-18 09:14:26 -0500

    Remove test/debug code.

--- apps/browser/src/popup/app-routing.module.ts
+++ apps/browser/src/popup/app-routing.module.ts
@@ -447,7 +447,6 @@ const routes: Routes = [
                     overlayY: "bottom",
                   },
                 ],
-                test: "test",
               },
             },
           ],
