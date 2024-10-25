e4d8678b9dedd9231b8323fdbc4d255265034833
✨ Audrey ✨ <audrey@audreyality.com>
2024-10-25 12:16:48 -0400

    add minimal reproduction

--- apps/browser/src/tools/popup/generator/credential-generator.component.ts
+++ apps/browser/src/tools/popup/generator/credential-generator.component.ts
@@ -1,30 +1,11 @@
 import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
 
-import { JslibModule } from "@bitwarden/angular/jslib.module";
-import { ItemModule } from "@bitwarden/components";
-import { GeneratorModule } from "@bitwarden/generator-components";
-
-import { CurrentAccountComponent } from "../../../auth/popup/account-switching/current-account.component";
-import { PopOutComponent } from "../../../platform/popup/components/pop-out.component";
-import { PopupFooterComponent } from "../../../platform/popup/layout/popup-footer.component";
-import { PopupHeaderComponent } from "../../../platform/popup/layout/popup-header.component";
-import { PopupPageComponent } from "../../../platform/popup/layout/popup-page.component";
+import { ValidationTestComponent } from "@bitwarden/generator-components";
 
 @Component({
   standalone: true,
   selector: "credential-generator",
   templateUrl: "credential-generator.component.html",
-  imports: [
-    GeneratorModule,
-    CurrentAccountComponent,
-    JslibModule,
-    PopOutComponent,
-    PopupHeaderComponent,
-    PopupPageComponent,
-    PopupFooterComponent,
-    RouterModule,
-    ItemModule,
-  ],
+  imports: [ValidationTestComponent],
 })
 export class CredentialGeneratorComponent {}
---
+++ libs/tools/generator/components/src/validation-test.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Subject, takeUntil } from "rxjs";
+
+import { FormFieldModule, InputModule } from "@bitwarden/components";
+
+/** Minimal test case for validation not blocking `form.valueChanges`
+ * @deprecated this type should not be used anywhere
+ * */
+@Component({
+  selector: "tools-validation-test",
+  templateUrl: "validation-test.component.html",
+  standalone: true,
+  imports: [FormFieldModule, InputModule, ReactiveFormsModule],
+})
+export class ValidationTestComponent implements OnInit, OnDestroy {
+  constructor(private formBuilder: FormBuilder) {}
+
+  protected form = this.formBuilder.group({
+    someNumber: [null as number],
+    someText: [null as string],
+  });
+
+  ngOnInit(): void {
+    this.form.get("someNumber").setValidators([Validators.max(10), Validators.min(4)]);
+
+    this.form.get("someText").setValidators([Validators.required]);
+
+    /* eslint no-console: 0 */
+    this.form.valueChanges.pipe(takeUntil(this.destroyed$)).subscribe((v) => console.log(v));
+  }
+
+  private readonly destroyed$ = new Subject<void>();
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+}
