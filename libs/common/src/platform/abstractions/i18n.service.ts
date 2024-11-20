edcb95f4c0018ed7d34b7a9cd42c979af38f8a7f
William Martin <contact@willmartian.com>
2024-11-20 10:52:19 -0500

    use any type as default generic for piecemeal migration

--- libs/common/src/platform/abstractions/i18n.service.ts
+++ libs/common/src/platform/abstractions/i18n.service.ts
@@ -2,7 +2,8 @@ import { Observable } from "rxjs";
 
 import { ClientTuple, TranslationService } from "./translation.service";
 
-export abstract class I18nService<T extends ClientTuple> extends TranslationService<T> {
+/** TODO: remove default generic of `any` */
+export abstract class I18nService<T extends ClientTuple = any> extends TranslationService<T> {
   abstract userSetLocale$: Observable<string | undefined>;
   abstract locale$: Observable<string>;
   abstract setLocale(locale: string): Promise<void>;
