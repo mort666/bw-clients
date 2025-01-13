4555e95323ad2842b46d3b24def2248546cb9419
Bernd Schoolmann <mail@quexten.com>
2025-01-13 17:06:57 +0100

    Fix biometrics not working in firefox or windows

--- apps/browser/src/key-management/biometrics/background-browser-biometrics.service.ts
+++ apps/browser/src/key-management/biometrics/background-browser-biometrics.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from "@angular/core";
 
 import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
+import { Utils } from "@bitwarden/common/platform/misc/utils";
+import { SymmetricCryptoKey } from "@bitwarden/common/platform/models/domain/symmetric-crypto-key";
 import { UserId } from "@bitwarden/common/types/guid";
 import { UserKey } from "@bitwarden/common/types/key";
-import { BiometricsService, BiometricsCommands, BiometricsStatus } from "@bitwarden/key-management";
+import {
+  BiometricsService,
+  BiometricsCommands,
+  BiometricsStatus,
+  KeyService,
+  BiometricStateService,
+} from "@bitwarden/key-management";
 
 import { NativeMessagingBackground } from "../../background/nativeMessaging.background";
 import { BrowserApi } from "../../platform/browser/browser-api";
@@ -13,6 +21,8 @@ export class BackgroundBrowserBiometricsService extends BiometricsService {
   constructor(
     private nativeMessagingBackground: () => NativeMessagingBackground,
     private logService: LogService,
+    private keyService: KeyService,
+    private biometricStateService: BiometricStateService,
   ) {
     super();
   }
@@ -75,21 +85,41 @@ export class BackgroundBrowserBiometricsService extends BiometricsService {
       await this.ensureConnected();
 
       if (this.nativeMessagingBackground().isConnectedToOutdatedDesktopClient) {
+        this.logService.info("Biometric unlock for user outdated", userId);
         const response = await this.nativeMessagingBackground().callCommand({
           command: BiometricsCommands.Unlock,
         });
+        this.logService.info("Biometric unlock for user", response);
         if (response.response == "unlocked") {
-          return response.userKeyB64;
+          const decodedUserkey = Utils.fromB64ToArray(response.userKeyB64);
+          const userKey = new SymmetricCryptoKey(decodedUserkey) as UserKey;
+          if (this.keyService.validateUserKey(userKey, userId)) {
+            this.logService.info("validated setting enabled");
+            await this.biometricStateService.setBiometricUnlockEnabled(true);
+            await this.biometricStateService.setFingerprintValidated(true);
+            this.keyService.setUserKey(userKey, userId);
+            return userKey;
+          }
         } else {
           return null;
         }
       } else {
+        this.logService.info("Unlock for new user", userId);
         const response = await this.nativeMessagingBackground().callCommand({
           command: BiometricsCommands.UnlockWithBiometricsForUser,
           userId: userId,
         });
+        this.logService.info("Biometric unlock for user1", response);
         if (response.response) {
-          return response.userKeyB64;
+          const decodedUserkey = Utils.fromB64ToArray(response.userKeyB64);
+          const userKey = new SymmetricCryptoKey(decodedUserkey) as UserKey;
+          if (this.keyService.validateUserKey(userKey, userId)) {
+            this.logService.info("validated setting enabled");
+            await this.biometricStateService.setBiometricUnlockEnabled(true);
+            await this.biometricStateService.setFingerprintValidated(true);
+            this.keyService.setUserKey(userKey, userId);
+            return userKey;
+          }
         } else {
           return null;
         }
