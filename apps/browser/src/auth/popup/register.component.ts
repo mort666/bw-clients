1e1a2baa17e2c3118b405e08c7f1286d85bd9cf0
Bernd Schoolmann <mail@quexten.com>
2025-01-13 12:14:14 +0100

    Fix build on browser

--- apps/browser/src/auth/popup/register.component.ts
+++ apps/browser/src/auth/popup/register.component.ts
@@ -9,6 +9,7 @@ import { FormValidationErrorsService } from "@bitwarden/angular/platform/abstrac
 import { LoginStrategyServiceAbstraction } from "@bitwarden/auth/common";
 import { ApiService } from "@bitwarden/common/abstractions/api.service";
 import { AuditService } from "@bitwarden/common/abstractions/audit.service";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { EnvironmentService } from "@bitwarden/common/platform/abstractions/environment.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
@@ -42,6 +43,7 @@ export class RegisterComponent extends BaseRegisterComponent {
     auditService: AuditService,
     dialogService: DialogService,
     toastService: ToastService,
+    configService: ConfigService,
   ) {
     super(
       formValidationErrorService,
@@ -59,6 +61,7 @@ export class RegisterComponent extends BaseRegisterComponent {
       auditService,
       dialogService,
       toastService,
+      configService,
     );
   }
 }
