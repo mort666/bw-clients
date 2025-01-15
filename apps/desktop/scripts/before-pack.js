26d93f430b4d75e05c30712d4f7ff5458c76cf43
Jake Fink <jfink@bitwarden.com>
2025-01-14 23:40:40 -0500

    only use before pack on snap build

--- apps/desktop/scripts/before-pack.js
+++ apps/desktop/scripts/before-pack.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-require-imports, no-console */
 
 const path = require("path");
-const { inspect } = require("util");
 
 const { Platform } = require("electron-builder");
 const fse = require("fs-extra");
