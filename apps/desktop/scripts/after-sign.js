71e3b8a9f66d975f1e7344ffecf1d7e6b1720b2b
Anders Åberg <anders@andersaberg.com>
2025-02-18 10:01:41 +0100

    skip notarization

--- apps/desktop/scripts/after-sign.js
+++ apps/desktop/scripts/after-sign.js
@@ -75,7 +75,7 @@ async function run(context) {
     }
   }
 
-  if (macBuild  || copyAutofillExtension) {
+  if (macBuild) {
     console.log("### Notarizing " + appPath);
     if (process.env.APP_STORE_CONNECT_TEAM_ISSUER) {
       const appleApiIssuer = process.env.APP_STORE_CONNECT_TEAM_ISSUER;
@@ -99,5 +99,7 @@ async function run(context) {
         appleIdPassword: appleIdPassword,
       });
     }
+  } else {
+    console.log("### Skipping notarization");
   }
 }
