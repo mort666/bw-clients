a61756bc65c757ff4cd727e401821a5539d205e7
Nick Krantz <nick@livefront.com>
2025-02-27 14:40:30 -0600

    remove icons from new device verification guard

--- libs/vault/src/icons/index.ts
+++ libs/vault/src/icons/index.ts
@@ -2,7 +2,5 @@ export * from "./deactivated-org";
 export * from "./no-folders";
 export * from "./vault";
 export * from "./empty-trash";
-export * from "./exclamation-triangle";
-export * from "./user-lock";
 export * from "./browser-extension";
 export * from "./bitwarden-icon";
