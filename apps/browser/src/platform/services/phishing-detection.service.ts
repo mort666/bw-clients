c946311d714217a1c3061330dee2e16dae5a8e0d
Jimmy Vo <huynhmaivo82@gmail.com>
2025-03-12 16:54:06 -0400

    Add a tab listener.

--- apps/browser/src/platform/services/phishing-detection.service.ts
+++ apps/browser/src/platform/services/phishing-detection.service.ts
@@ -12,21 +12,45 @@ export class PhishingDetectionService {
 
   // @TODO: We need to flesh this out to actually use the real data that comes from the server.
   // This method can be run using a background worker once a day or at a similar interval.
-  static updateKnownPhishingDomains(): void {
-    PhishingDetectionService.loadMockedData();
-  }
+  static updateKnownPhishingDomains(): void {}
 
   // @TODO: This can be remove once we implement the real code.
-  private static loadMockedData() {
+  static loadMockedData() {
     PhishingDetectionService.knownPhishingDomains.add("google.com");
     PhishingDetectionService.knownPhishingDomains.add("atlassian.net");
   }
 
-  async getActiveUrl(): Promise<string> {
+  static async getActiveUrl(): Promise<string> {
     const win = await BrowserApi.getCurrentWindow();
     const currentWindow = await BrowserApi.tabsQuery({ windowId: win.id, active: true });
 
     // @TODO: Account for cases with no active windows.
     return currentWindow[0].url;
   }
+
+  // @TODO: WIP. We can have a pop-up or send a notification to other services.
+  static notifyUser(url: string) {}
+
+  /*
+    This listener will check the URL when the tab has finished loading.
+  */
+  setupTabEventListeners(): void {
+    BrowserApi.addListener(chrome.tabs.onUpdated, async (tabId, changeInfo, tab) => {
+      if (changeInfo.status === "complete") {
+        const activeUrl = await PhishingDetectionService.getActiveUrl();
+
+        // Debugging
+        console.log("Tab changed:", { tab, changeInfo, tabId });
+
+        const isPhishingDomain = PhishingDetectionService.checkUrl(activeUrl);
+
+        if (isPhishingDomain) {
+          PhishingDetectionService.notifyUser(activeUrl);
+        }
+      }
+    });
+  }
 }
+
+// Initializing the data for local development
+PhishingDetectionService.loadMockedData();
