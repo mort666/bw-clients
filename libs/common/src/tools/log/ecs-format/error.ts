7589692fb100db55f9b59f835dc580ba70ede9c0
✨ Audrey ✨ <audrey@audreyality.com>
2025-03-12 10:32:48 -0400

    prettier

--- libs/common/src/tools/log/ecs-format/error.ts
+++ libs/common/src/tools/log/ecs-format/error.ts
@@ -4,12 +4,12 @@ export type ErrorFormat = EcsFormat & {
   /** Error indicators collected by the provider */
   error: {
     /** content from the message field of the error */
-    message: string,
+    message: string;
 
     /** content from the error's stack trace */
-    stack_trace: string,
+    stack_trace: string;
 
     /** the type of the error, for example the error's class name */
-    type: string
-  },
+    type: string;
+  };
 };
--- libs/common/src/tools/log/ecs-format/log.ts
+++ libs/common/src/tools/log/ecs-format/log.ts
@@ -4,13 +4,13 @@ export type LogFormat = EcsFormat & {
   /** Log metadata */
   log: {
     /** original log level of the event */
-    level: "debug" | "info" | "warn" | "error",
+    level: "debug" | "info" | "warn" | "error";
 
     /** source of the event; this is usually a type name */
-    logger: string,
+    logger: string;
 
     // FIXME: if it becomes possible to include line/file numbers,
     // add the origin fields from here:
     //   https://www.elastic.co/guide/en/ecs/current/ecs-log.html
-  }
-}
+  };
+};
