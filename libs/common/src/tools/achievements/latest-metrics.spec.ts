d7909bd87a073a7457b35a6359c62725febdcc42
✨ Audrey ✨ <audrey@audreyality.com>
2025-03-19 10:10:10 -0400

    fix unit tests

--- libs/common/src/tools/achievements/latest-metrics.spec.ts
+++ libs/common/src/tools/achievements/latest-metrics.spec.ts
@@ -12,55 +12,45 @@ import { AchievementProgressEvent, MetricId } from "./types";
 describe("latestMetrics", () => {
   it("creates a map containing a metric", () => {
     const subject = new Subject<AchievementProgressEvent>();
-    const result = new BehaviorSubject(new Map<MetricId, number>());
+    const result = new BehaviorSubject(new Map<MetricId, AchievementProgressEvent>());
 
     subject.pipe(latestProgressMetrics()).subscribe(result);
     subject.next(ItemCreatedProgressEvent);
 
-    expect(result.value.get(ItemCreatedProgress)).toEqual(
-      ItemCreatedProgressEvent.achievement.value,
-    );
+    expect(result.value.get(ItemCreatedProgress)).toEqual(ItemCreatedProgressEvent);
   });
 
   it("creates a map containing multiple metrics", () => {
     const subject = new Subject<AchievementProgressEvent>();
-    const result = new BehaviorSubject(new Map<MetricId, number>());
+    const result = new BehaviorSubject(new Map<MetricId, AchievementProgressEvent>());
 
     subject.pipe(latestProgressMetrics()).subscribe(result);
     subject.next(ItemCreatedProgressEvent);
     subject.next(CredentialGeneratedProgressEvent);
 
-    expect(result.value.get(ItemCreatedProgress)).toEqual(
-      ItemCreatedProgressEvent.achievement.value,
-    );
-    expect(result.value.get(CredentialGeneratedProgress)).toEqual(
-      CredentialGeneratedProgressEvent.achievement.value,
-    );
+    expect(result.value.get(ItemCreatedProgress)).toEqual(ItemCreatedProgressEvent);
+    expect(result.value.get(CredentialGeneratedProgress)).toEqual(CredentialGeneratedProgressEvent);
   });
 
   it("creates a map containing updated metrics", () => {
     const subject = new Subject<AchievementProgressEvent>();
-    const result = new BehaviorSubject(new Map<MetricId, number>());
+    const result = new BehaviorSubject(new Map<MetricId, AchievementProgressEvent>());
 
     subject.pipe(latestProgressMetrics()).subscribe(result);
     subject.next(ItemCreatedProgressEvent);
     subject.next(ItemCreatedProgress2Event);
 
-    expect(result.value.get(ItemCreatedProgress)).toEqual(
-      ItemCreatedProgress2Event.achievement.value,
-    );
+    expect(result.value.get(ItemCreatedProgress)).toEqual(ItemCreatedProgress2Event);
   });
 
   it("omits old events", () => {
     const subject = new Subject<AchievementProgressEvent>();
-    const result = new BehaviorSubject(new Map<MetricId, number>());
+    const result = new BehaviorSubject(new Map<MetricId, AchievementProgressEvent>());
 
     subject.pipe(latestProgressMetrics()).subscribe(result);
     subject.next(ItemCreatedProgress2Event);
     subject.next(ItemCreatedProgressEvent);
 
-    expect(result.value.get(ItemCreatedProgress)).toEqual(
-      ItemCreatedProgress2Event.achievement.value,
-    );
+    expect(result.value.get(ItemCreatedProgress)).toEqual(ItemCreatedProgress2Event);
   });
 });
