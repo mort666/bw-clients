85e3f0612b02856d945cebb2917a8e4d23cb6553
Jeffrey Holland <jholland@livefront.com>
2025-04-02 19:57:09 +0200

    Import OnDestroy correctly

--- apps/desktop/src/modal/passkeys/create/fido2-create.component.ts
+++ apps/desktop/src/modal/passkeys/create/fido2-create.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { RouterModule, Router } from "@angular/router";
 import { BehaviorSubject, firstValueFrom, map, Observable } from "rxjs";
 
