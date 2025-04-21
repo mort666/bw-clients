74496d49ce7b56867906aae4d390304b78c1f7b8
✨ Audrey ✨ <audrey@audreyality.com>
2025-04-21 13:09:24 -0400

    annotate tech debt

--- libs/tools/generator/core/src/types/generated-credential.ts
+++ libs/tools/generator/core/src/types/generated-credential.ts
@@ -7,7 +7,7 @@ export class GeneratedCredential {
   /**
    * Instantiates a generated credential
    * @param credential The value of the generated credential (e.g. a password)
-   * @param category The kind of credential
+   * @param category The type of credential
    * @param generationDate The date that the credential was generated.
    *   Numeric values should are interpreted using {@link Date.valueOf}
    *   semantics.
@@ -16,6 +16,8 @@ export class GeneratedCredential {
    */
   constructor(
     readonly credential: string,
+    // FIXME: create a way to migrate the data stored in `category` to a new `type`
+    //   field. The hard part: This requires the migration occur post-decryption.
     readonly category: CredentialType,
     generationDate: Date | number,
     readonly source?: string,
