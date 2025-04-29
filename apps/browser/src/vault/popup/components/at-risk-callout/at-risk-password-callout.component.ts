a6866a83791608ab2fc6a18597b3f88fa10d3326
Nick Krantz <nick@livefront.com>
2025-04-29 16:44:11 -0500

    use `atRiskItems$` for the at-risk-password callout

--- apps/browser/src/vault/popup/components/at-risk-callout/at-risk-password-callout.component.ts
+++ apps/browser/src/vault/popup/components/at-risk-callout/at-risk-password-callout.component.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { map, of, switchMap } from "rxjs";
 
-import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
-import { getUserId } from "@bitwarden/common/auth/services/account.service";
-import { SecurityTaskType, TaskService } from "@bitwarden/common/vault/tasks";
 import { AnchorLinkDirective, CalloutModule } from "@bitwarden/components";
 import { I18nPipe } from "@bitwarden/ui-common";
+
+import { AtRiskPasswordPageService } from "../at-risk-passwords/at-risk-password-page.service";
 
 @Component({
   selector: "vault-at-risk-password-callout",
@@ -16,25 +14,7 @@ import { I18nPipe } from "@bitwarden/ui-common";
   templateUrl: "./at-risk-password-callout.component.html",
 })
 export class AtRiskPasswordCalloutComponent {
-  private taskService = inject(TaskService);
-  private activeAccount$ = inject(AccountService).activeAccount$.pipe(getUserId);
+  private atRiskPasswordPageService = inject(AtRiskPasswordPageService);
 
-  protected pendingTasks$ = this.activeAccount$.pipe(
-    switchMap((userId) =>
-      this.taskService.tasksEnabled$(userId).pipe(
-        switchMap((enabled) => {
-          if (!enabled) {
-            return of([]);
-          }
-          return this.taskService
-            .pendingTasks$(userId)
-            .pipe(
-              map((tasks) =>
-                tasks.filter((t) => t.type === SecurityTaskType.UpdateAtRiskCredential),
-              ),
-            );
-        }),
-      ),
-    ),
-  );
+  protected atRiskItems$ = this.atRiskPasswordPageService.atRiskItems$;
 }
