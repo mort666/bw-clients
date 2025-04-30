eec191fd81d6058a1ab48c3bc06981def8ee94af
Anders Åberg <anders@andersaberg.com>
2025-04-30 18:57:02 +0200

    Revert "try to force sdk version"
    This reverts commit d94f2550adf69d944b49273221dbcdcf1a53e416.

--- apps/desktop/scripts/build-macos-extension.js
+++ apps/desktop/scripts/build-macos-extension.js
@@ -64,8 +64,6 @@ async function buildMacOs() {
     "-alltargets",
     "-configuration",
     configuration,
-    "-sdk",
-    "macosx15.0", // Specify the SDK version explicitly
     "CODE_SIGN_INJECT_BASE_ENTITLEMENTS=NO",
     "OTHER_CODE_SIGN_FLAGS='--timestamp'",
 
