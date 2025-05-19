a04b2071a7095a9e96e3d176f742a27da2c14deb
Bernd Schoolmann <mail@quexten.com>
2025-05-19 12:37:23 +0200

    Add more docs

--- libs/common/src/key-management/types.ts
+++ libs/common/src/key-management/types.ts
@@ -1,2 +1,7 @@
-/// A base64 encoded proof that a signing key identity claims ownership of a public encryption key.
+/**
+ * A base64 encoded proof that a signing key identity claims ownership of a public encryption key
+ * Specifically, a claimer, signs a message with their signing key, where the message says (in part):
+ *  - "I own publicKey XYZ"
+ * This shows that a user/signing identity wants to receive confidential data to this public key.
+ */
 export type SignedPublicKeyOwnershipClaim = string;
