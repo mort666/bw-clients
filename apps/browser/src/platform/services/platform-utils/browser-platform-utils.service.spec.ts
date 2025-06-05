4d06ceb8fb88588d31bbdfd5af2b790d8aa91be2
Todd Martin <tmartin@bitwarden.com>
2025-06-05 16:24:24 -0400

    Add test for DDG

--- apps/browser/src/platform/services/platform-utils/browser-platform-utils.service.spec.ts
+++ apps/browser/src/platform/services/platform-utils/browser-platform-utils.service.spec.ts
@@ -121,6 +121,16 @@ describe("Browser Utils Service", () => {
       expect(browserPlatformUtilsService.getDevice()).toBe(DeviceType.VivaldiExtension);
     });
 
+    it("should detect duckduckgo", () => {
+      Object.defineProperty(navigator, "userAgent", {
+        configurable: true,
+        value:
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 Safari/605.1.15 Ddg/18.4",
+      });
+
+      expect(browserPlatformUtilsService.getDevice()).toBe(DeviceType.DuckDuckGoExtension);
+    });
+
     it("returns a previously determined device using a cached value", () => {
       Object.defineProperty(navigator, "userAgent", {
         configurable: true,
