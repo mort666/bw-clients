e09baeabb194656d91fcc1ebcab8cbf142216aa4
Bernd Schoolmann <mail@quexten.com>
2025-07-16 15:36:44 +0200

    Add newline

--- libs/common/src/key-management/master-password/types/master-password.types.ts
+++ libs/common/src/key-management/master-password/types/master-password.types.ts
@@ -1,6 +1,8 @@
 import { Opaque } from "type-fest";
-import { KdfConfig } from "@bitwarden/key-management";
+
 import { EncString } from "@bitwarden/common/platform/models/domain/enc-string";
+// eslint-disable-next-line no-restricted-imports
+import { KdfConfig } from "@bitwarden/key-management";
 
 /**
  * The Base64-encoded master password authentication hash, that is sent to the server for authentication.
@@ -10,19 +12,19 @@ export type MasterPasswordSalt = Opaque<string, "MasterPasswordSalt">;
 export type MasterKeyWrappedUserKey = Opaque<EncString, "MasterPasswordSalt">;
 
 /**
- * The data required to unlock the master password. 
+ * The data required to unlock the master password.
  */
 export type MasterPasswordUnlockData = {
-    salt: string;
-    kdf: KdfConfig;
-    masterKeyWrappedUserKey: MasterKeyWrappedUserKey;
-}
+  salt: string;
+  kdf: KdfConfig;
+  masterKeyWrappedUserKey: MasterKeyWrappedUserKey;
+};
 
 /**
- * The data required to unlock the master password. 
+ * The data required to unlock the master password.
  */
 export type MasterPasswordAuthenticationData = {
-    salt: string;
-    kdf: KdfConfig;
-    masterPasswordAuthenticationHash: MasterPasswordAuthenticationHash;
-} 
+  salt: string;
+  kdf: KdfConfig;
+  masterPasswordAuthenticationHash: MasterPasswordAuthenticationHash;
+};
