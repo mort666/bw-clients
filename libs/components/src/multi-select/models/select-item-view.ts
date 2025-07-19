ad0baec404070d7cb560eb869ff7b0cd606e680c
Thomas Rittson <trittson@bitwarden.com>
2025-07-19 10:48:30 +1000

    Clarify SelectItemView changes

--- libs/components/src/multi-select/models/select-item-view.ts
+++ libs/components/src/multi-select/models/select-item-view.ts
@@ -1,15 +1,14 @@
 import { CollectionId } from "@bitwarden/common/types/guid";
 
-export type SelectItemView = {
-  id: string; // Unique ID used for comparisons
-  listName: string; // Default bindValue -> this is what will be displayed in list items
-  labelName: string; // This is what will be displayed in the selection option badge
-  icon?: string; // Icon to display within the list
-  parentGrouping?: string; // Used to group items by parent
+export type SelectItemView = BaseSelectView & {
+  id: string; // Unique ID used for comparisons - this may represent different object IDs depending on the context
 };
 
-export type SelectCollectionView = {
-  id: CollectionId; // Unique ID used for comparisons
+export type SelectCollectionView = BaseSelectView & {
+  id: CollectionId;
+};
+
+export type BaseSelectView = {
   listName: string; // Default bindValue -> this is what will be displayed in list items
   labelName: string; // This is what will be displayed in the selection option badge
   icon?: string; // Icon to display within the list
