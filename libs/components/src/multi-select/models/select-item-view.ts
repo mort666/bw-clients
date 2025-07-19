e4db22a87069f8de238ef55550f38cfafea1376e
Thomas Rittson <trittson@bitwarden.com>
2025-07-19 10:52:55 +1000

    Don't export type

--- libs/components/src/multi-select/models/select-item-view.ts
+++ libs/components/src/multi-select/models/select-item-view.ts
@@ -8,7 +8,7 @@ export type SelectCollectionView = BaseSelectView & {
   id: CollectionId;
 };
 
-export type BaseSelectView = {
+type BaseSelectView = {
   listName: string; // Default bindValue -> this is what will be displayed in list items
   labelName: string; // This is what will be displayed in the selection option badge
   icon?: string; // Icon to display within the list
