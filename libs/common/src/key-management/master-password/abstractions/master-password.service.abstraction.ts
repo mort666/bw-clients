6ac261b6476e50fb622835d37ed487a0ec702b35
Bernd Schoolmann <mail@quexten.com>
2025-07-22 12:54:40 +0200

    Update libs/common/src/key-management/master-password/abstractions/master-password.service.abstraction.ts
    Co-authored-by: Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>

--- libs/common/src/key-management/master-password/abstractions/master-password.service.abstraction.ts
+++ libs/common/src/key-management/master-password/abstractions/master-password.service.abstraction.ts
@@ -22,7 +22,7 @@ export abstract class MasterPasswordServiceAbstraction {
   abstract forceSetPasswordReason$: (userId: UserId) => Observable<ForceSetPasswordReason>;
   /**
    * An observable that emits the master key for the user.
-   * @deprecated Interacting with the master-key directly is deprecated. Please use {@link makeMasterPasswordUnlockData} and {@link makeMasterPasswordAuthenticationData}, {@link unwrapUserKeyFromMasterPasswordUnlockData} instead.
+   * @deprecated Interacting with the master-key directly is deprecated. Please use {@link makeMasterPasswordUnlockData}, {@link makeMasterPasswordAuthenticationData} or {@link unwrapUserKeyFromMasterPasswordUnlockData} instead.
    * @param userId The user ID.
    * @throws If the user ID is missing.
    */
