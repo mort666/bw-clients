0301fb3855ce4721ba129586b01e3add163e1798
Thomas Rittson <trittson@bitwarden.com>
2025-07-25 14:27:57 +1000

    Resolve merge conflicts

--- libs/admin-console/src/common/collections/models/collection.ts
+++ libs/admin-console/src/common/collections/models/collection.ts
@@ -1,10 +1,6 @@
 import { EncString } from "@bitwarden/common/key-management/crypto/models/enc-string";
-<<<<<<< HEAD
-import Domain from "@bitwarden/common/platform/models/domain/domain-base";
-import { CollectionId, OrganizationId } from "@bitwarden/common/types/guid";
-=======
 import Domain, { EncryptableKeys } from "@bitwarden/common/platform/models/domain/domain-base";
->>>>>>> origin/main
+import { CollectionId, OrganizationId } from "@bitwarden/common/types/guid";
 import { OrgKey } from "@bitwarden/common/types/key";
 
 import { CollectionData } from "./collection.data";
@@ -18,25 +14,14 @@ export const CollectionTypes = {
 export type CollectionType = (typeof CollectionTypes)[keyof typeof CollectionTypes];
 
 export class Collection extends Domain {
-<<<<<<< HEAD
-  id: CollectionId;
-  organizationId: OrganizationId;
-  name: EncString;
-  externalId: string;
-  readOnly: boolean;
-  hidePasswords: boolean;
-  manage: boolean;
-  type: CollectionType;
-=======
-  id: string | undefined;
-  organizationId: string | undefined;
+  id: CollectionId | undefined;
+  organizationId: OrganizationId | undefined;
   name: EncString | undefined;
   externalId: string | undefined;
   readOnly: boolean = false;
   hidePasswords: boolean = false;
   manage: boolean = false;
   type: CollectionType = CollectionTypes.SharedCollection;
->>>>>>> origin/main
 
   constructor(obj?: CollectionData | null) {
     super();
