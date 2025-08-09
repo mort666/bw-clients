63446fda8188a59dbe3a056b8c01e97d50aac773
addisonbeck <github@addisonbeck.com>
2025-08-09 12:21:31 -0400

    refactor: introduce @bitwarden/platform-utils

---
+++ libs/platform-utils/README.md
@@ -0,0 +1,5 @@
+# platform-utils
+
+Owned by: platform
+
+Assorted utils for working with toasts, device type, clipboard, etc. This library needs to be broken up
---
+++ libs/platform-utils/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: "platform-utils",
+  preset: "../../jest.preset.js",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
+  },
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageDirectory: "../../coverage/libs/platform-utils",
+};
