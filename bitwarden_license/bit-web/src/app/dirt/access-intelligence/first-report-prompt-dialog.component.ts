78b7d85e52f0bfad3cf5a83598c7a67252f4f344
Alex <adewitt@bitwarden.com>
2025-09-11 12:40:04 -0400

    bitLink for styling

--- bitwarden_license/bit-web/src/app/dirt/access-intelligence/first-report-prompt-dialog.component.ts
+++ bitwarden_license/bit-web/src/app/dirt/access-intelligence/first-report-prompt-dialog.component.ts
@@ -1,11 +1,17 @@
 import { Component } from "@angular/core";
 
-import { ButtonModule, DialogModule, DialogService, TypographyModule } from "@bitwarden/components";
+import {
+  ButtonModule,
+  DialogModule,
+  DialogService,
+  LinkModule,
+  TypographyModule,
+} from "@bitwarden/components";
 import { I18nPipe } from "@bitwarden/ui-common";
 
 @Component({
   templateUrl: "./first-report-prompt-dialog.component.html",
-  imports: [ButtonModule, DialogModule, TypographyModule, I18nPipe],
+  imports: [ButtonModule, DialogModule, LinkModule, TypographyModule, I18nPipe],
 })
 export class FirstReportPromptDialogComponent {
   static open(dialogService: DialogService) {
