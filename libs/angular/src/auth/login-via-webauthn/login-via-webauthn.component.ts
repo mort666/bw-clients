9860ae1d56309215e45af3de10af73a4dff01ff5
Anders Åberg <anders@andersaberg.com>
2025-09-17 11:33:36 +0200

    Remove default Icons on load

--- libs/angular/src/auth/login-via-webauthn/login-via-webauthn.component.ts
+++ libs/angular/src/auth/login-via-webauthn/login-via-webauthn.component.ts
@@ -78,8 +78,6 @@ export class LoginViaWebAuthnComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // Set default icon for the component
-    this.setDefaultIcon();
     // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     this.authenticate();
