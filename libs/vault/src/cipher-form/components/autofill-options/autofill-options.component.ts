1dc2bb440d5e7284825e8cd035016c5d29427265
jaasen-livefront <jaasen@livefront.com>
2025-09-25 16:47:49 -0700

    handle parenthesis translation

--- libs/vault/src/cipher-form/components/autofill-options/autofill-options.component.ts
+++ libs/vault/src/cipher-form/components/autofill-options/autofill-options.component.ts
@@ -212,7 +212,9 @@ export class AutofillOptionsComponent implements OnInit {
           return;
         }
 
-        this.autofillOptions[0].label = this.i18nService.t("defaultLabel", defaultOption.label);
+        // Safari is unable to format messages with placeholders surrounded by parentheses manually inserting them is required
+        this.autofillOptions[0].label =
+          this.i18nService.t("default") + " (" + defaultOption.label + ")";
         // Trigger change detection to update the label in the template
         this.autofillOptions = [...this.autofillOptions];
       });
