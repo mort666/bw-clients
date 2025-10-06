ae7124b4836ef62ab61947dff7be3235e02d63b1
Ben Brooks <bbrooks@bitwarden.com>
2025-10-06 10:27:38 -0700

    Move options to component class and use values from domain-service
    Signed-off-by: Ben Brooks <bbrooks@bitwarden.com>

--- apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/uri-match-default.component.ts
+++ apps/web/src/app/admin-console/organizations/policies/policy-edit-definitions/uri-match-default.component.ts
@@ -5,6 +5,10 @@ import { PolicyType } from "@bitwarden/common/admin-console/enums";
 // import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
 // import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
 // import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
+import {
+  UriMatchStrategy,
+  UriMatchStrategySetting,
+} from "@bitwarden/common/models/domain/domain-service";
 
 import { SharedModule } from "../../../../shared";
 import { BasePolicyEditDefinition, BasePolicyEditComponent } from "../base-policy-edit.component";
@@ -15,21 +19,23 @@ export class UriMatchDefaultPolicy extends BasePolicyEditDefinition {
   type = PolicyType.UriMatchDefaults;
   component = UriMatchDefaultPolicyComponent;
 
+  // TODO: Remove if we won't use a feature flag
   // display$(organization: Organization, configService: ConfigService) {
   //   return configService.getFeatureFlag$(FeatureFlag.WindowsDesktopAutotype);
   // }
-
-  uriMatchOptions: { name: string; value: string }[] = [
-    // TODO: Should this be integers?
-    { name: "-- " + "Select" + " --", value: null },
-    { name: "Exact", value: "exact" },
-    { name: "Base Domain", value: "base_domain" },
-    { name: "Host", value: "host" },
-    { name: "Starts With", value: "starts_with" },
-  ];
 }
 @Component({
   templateUrl: "uri-match-default.component.html",
   imports: [SharedModule],
 })
-export class UriMatchDefaultPolicyComponent extends BasePolicyEditComponent {}
+export class UriMatchDefaultPolicyComponent extends BasePolicyEditComponent {
+  uriMatchOptions: { name: string; value: UriMatchStrategySetting | null }[] = [
+    { name: "-- Select --", value: null },
+    { name: "Base Domain", value: UriMatchStrategy.Domain },
+    { name: "Host", value: UriMatchStrategy.Host },
+    { name: "Starts With", value: UriMatchStrategy.StartsWith },
+    { name: "Exact", value: UriMatchStrategy.Exact },
+    { name: "Regular Expression", value: UriMatchStrategy.RegularExpression },
+    { name: "Never", value: UriMatchStrategy.Never },
+  ];
+}
