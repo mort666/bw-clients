39277be5930fd50fb6fc29cb927a2bfda1f1c595
Matt Gibson <mgibson@bitwarden.com>
2025-10-07 15:07:54 -0700

    Add web client as server

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,9 +72,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "cd apps/web && npm run build:bit:watch",
+    url: "https://localhost:8080 ",
+    reuseExistingServer: true, //!process.env.CI,
+  },
 });
