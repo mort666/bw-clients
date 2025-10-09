7ca565347f8fd4f3c2a5691bf2f75c0aa8a8bb91
Mark Youssef <myoussef@bitwarden.com>
2025-10-09 10:29:53 -0700

    Fix storybook position issue and debounce setting state

--- libs/components/src/navigation/side-nav.component.ts
+++ libs/components/src/navigation/side-nav.component.ts
@@ -1,9 +1,10 @@
 import { CdkTrapFocus } from "@angular/cdk/a11y";
 import { DragDropModule, CdkDragMove } from "@angular/cdk/drag-drop";
 import { CommonModule } from "@angular/common";
-import { Component, ElementRef, inject, input, OnDestroy, viewChild } from "@angular/core";
-import { Observable, Subject } from "rxjs";
-import { map, takeUntil } from "rxjs/operators";
+import { Component, ElementRef, inject, input, OnDestroy, signal, viewChild } from "@angular/core";
+import { toObservable } from "@angular/core/rxjs-interop";
+import { Subject } from "rxjs";
+import { debounceTime, map, takeUntil } from "rxjs/operators";
 
 import { BIT_SIDE_NAV_DISK, GlobalStateProvider, KeyDefinition } from "@bitwarden/state";
 import { I18nPipe } from "@bitwarden/ui-common";
@@ -39,24 +40,36 @@ export class SideNavComponent implements OnDestroy {
 
   private readonly toggleButton = viewChild("toggleButton", { read: ElementRef });
 
+  private elementRef = inject(ElementRef<HTMLElement>);
   protected lastOpenWidth = DEFAULT_OPEN_WIDTH;
-  private readonly state = inject(GlobalStateProvider).get(BIT_SIDE_NAV_WIDTH_KEY_DEF);
-  readonly width$: Observable<number> = this.state.state$.pipe(
-    map((state) => state ?? DEFAULT_OPEN_WIDTH),
-  );
+  private width = signal<number>(DEFAULT_OPEN_WIDTH);
+  protected width$ = toObservable(this.width);
 
-  async setWidth(width: number) {
-    await this.state.update(() => width);
-  }
+  private readonly widthState = inject(GlobalStateProvider).get(BIT_SIDE_NAV_WIDTH_KEY_DEF);
+  readonly widthState$ = this.widthState.state$.pipe(map((width) => width ?? DEFAULT_OPEN_WIDTH));
+  private debouncedWidthStateSubject = new Subject<number>();
 
   private readonly destroy$ = new Subject<void>();
 
   constructor(protected sideNavService: SideNavService) {
     this.width$.pipe(takeUntil(this.destroy$)).subscribe((width) => {
+      // Store the last open width when the side nav is open
       if (this.sideNavService.open) {
         this.lastOpenWidth = width;
       }
     });
+
+    // Initialize the width from state
+    void this.widthState$.forEach((width) => {
+      this.width.set(width);
+    });
+
+    // Debounce storing the width to avoid excessive writes to storage
+    this.debouncedWidthStateSubject
+      .pipe(debounceTime(200), takeUntil(this.destroy$))
+      .subscribe((width) => {
+        void this.widthState.update(() => width);
+      });
   }
 
   ngOnDestroy(): void {
@@ -75,7 +88,15 @@ export class SideNavComponent implements OnDestroy {
   };
 
   protected onDragMoved(event: CdkDragMove) {
-    void this.setWidth(Math.min(Math.max(event.pointerPosition.x, MIN_OPEN_WIDTH), MAX_OPEN_WIDTH));
+    const rect = this.elementRef.nativeElement.getBoundingClientRect();
+
+    const width = Math.min(
+      Math.max(event.pointerPosition.x - rect.x, MIN_OPEN_WIDTH),
+      MAX_OPEN_WIDTH,
+    );
+    this.width.set(width);
+    void this.debouncedWidthStateSubject.next(width);
+
     const element = event.source.element.nativeElement;
     element.style.transform = "none";
   }
