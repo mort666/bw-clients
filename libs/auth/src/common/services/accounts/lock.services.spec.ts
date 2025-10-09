d44230db8d3798fb217903c72e2a9d1323e8064f
Bernd Schoolmann <mail@quexten.com>
2025-10-09 14:07:53 +0200

    Fix tests

--- libs/auth/src/common/services/accounts/lock.services.spec.ts
+++ libs/auth/src/common/services/accounts/lock.services.spec.ts
@@ -1,8 +1,22 @@
 import { mock } from "jest-mock-extended";
 
-import { VaultTimeoutService } from "@bitwarden/common/key-management/vault-timeout";
+import { AuthService } from "@bitwarden/common/auth/abstractions/auth.service";
+import { TokenService } from "@bitwarden/common/auth/services/token.service";
+import { ProcessReloadServiceAbstraction } from "@bitwarden/common/key-management/abstractions/process-reload.service";
+import { InternalMasterPasswordServiceAbstraction } from "@bitwarden/common/key-management/master-password/abstractions/master-password.service.abstraction";
+import { VaultTimeoutSettingsService } from "@bitwarden/common/key-management/vault-timeout";
+import { MessagingService } from "@bitwarden/common/platform/abstractions/messaging.service";
+import { SystemService } from "@bitwarden/common/platform/abstractions/system.service";
 import { mockAccountServiceWith } from "@bitwarden/common/spec";
 import { UserId } from "@bitwarden/common/types/guid";
+import { CipherService } from "@bitwarden/common/vault/abstractions/cipher.service";
+import { FolderService } from "@bitwarden/common/vault/abstractions/folder/folder.service.abstraction";
+import { SearchService } from "@bitwarden/common/vault/abstractions/search.service";
+import { BiometricsService } from "@bitwarden/key-management";
+import { LogService } from "@bitwarden/logging";
+import { StateService, StateEventRunnerService } from "@bitwarden/state";
+
+import { LogoutService } from "../../abstractions";
 
 import { DefaultLockService } from "./lock.service";
 
@@ -12,9 +26,42 @@ describe("DefaultLockService", () => {
   const mockUser3 = "user3" as UserId;
 
   const accountService = mockAccountServiceWith(mockUser1);
-  const vaultTimeoutService = mock<VaultTimeoutService>();
+  const biometricsService = mock<BiometricsService>();
+  const tokenService = mock<TokenService>();
+  const vaultTimeoutSettingsService = mock<VaultTimeoutSettingsService>();
+  const logoutService = mock<LogoutService>();
+  const messagingService = mock<MessagingService>();
+  const searchService = mock<SearchService>();
+  const folderService = mock<FolderService>();
+  const masterPasswordService = mock<InternalMasterPasswordServiceAbstraction>();
+  const stateService = mock<StateService>();
+  const stateEventRunnerService = mock<StateEventRunnerService>();
+  const cipherService = mock<CipherService>();
+  const authService = mock<AuthService>();
+  const systemService = mock<SystemService>();
+  const processReloadService = mock<ProcessReloadServiceAbstraction>();
+  const logService = mock<LogService>();
 
-  const sut = new DefaultLockService(accountService, vaultTimeoutService);
+  const sut = new DefaultLockService(
+    accountService,
+    biometricsService,
+    tokenService,
+    vaultTimeoutSettingsService,
+    logoutService,
+    messagingService,
+    searchService,
+    folderService,
+    masterPasswordService,
+    stateService,
+    stateEventRunnerService,
+    cipherService,
+    authService,
+    systemService,
+    processReloadService,
+    biometricsService,
+    logService,
+  );
+
   describe("lockAll", () => {
     it("locks the active account last", async () => {
       await accountService.addAccount(mockUser2, {
@@ -25,19 +72,20 @@ describe("DefaultLockService", () => {
 
       await accountService.addAccount(mockUser3, {
         name: "name3",
-        email: "email3@example.com",
+        email: "name3@example.com",
         emailVerified: false,
       });
 
+      const lockSpy = jest.spyOn(sut, "lock").mockResolvedValue(undefined);
+
       await sut.lockAll();
 
-      expect(vaultTimeoutService.lock).toHaveBeenCalledTimes(3);
       // Non-Active users should be called first
-      expect(vaultTimeoutService.lock).toHaveBeenNthCalledWith(1, mockUser2);
-      expect(vaultTimeoutService.lock).toHaveBeenNthCalledWith(2, mockUser3);
+      expect(lockSpy).toHaveBeenNthCalledWith(1, mockUser2);
+      expect(lockSpy).toHaveBeenNthCalledWith(2, mockUser3);
 
       // Active user should be called last
-      expect(vaultTimeoutService.lock).toHaveBeenNthCalledWith(3, mockUser1);
+      expect(lockSpy).toHaveBeenNthCalledWith(3, mockUser1);
     });
   });
 });
