e3ab44ae95048093675af1b98bc873e8ba533a7a
neuronull <9162534+neuronull@users.noreply.github.com>
2025-10-17 14:27:34 -0600

    fix windows lint

--- apps/desktop/desktop_native/core/src/autofill/windows.rs
+++ apps/desktop/desktop_native/core/src/autofill/windows.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 /// # Errors
 ///
 /// TODO
-#[allow(clippy::unused_async, missing_errors_doc)]
+#[allow(clippy::unused_async)]
 pub async fn run_command(_value: String) -> Result<String> {
     todo!("Windows does not support autofill");
 }
