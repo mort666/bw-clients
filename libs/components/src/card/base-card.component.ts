ee3fe4ecd926c74c0e14aa0ad4192f46dbe7bd96
Bryan Cunningham <bryan.cunningham@me.com>
2025-10-21 10:04:18 -0400

    use base card directive on base component

--- libs/components/src/card/base-card.component.ts
+++ libs/components/src/card/base-card.component.ts
@@ -1,11 +1,10 @@
 import { Component } from "@angular/core";
 
+import { BaseCardDirective } from "./base-card.directive";
+
 @Component({
   selector: "bit-base-card",
   template: `<ng-content></ng-content>`,
-  host: {
-    class:
-      "tw-box-border tw-block tw-bg-background tw-text-main tw-border tw-border-solid tw-border-secondary-100 tw-shadow tw-rounded-xl",
-  },
+  hostDirectives: [BaseCardDirective],
 })
 export class BaseCardComponent {}
