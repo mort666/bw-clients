c30db87d31cf76628505875212658a1c6108e1a1
Hinton <hinton@users.noreply.github.com>
2025-10-22 19:38:48 -0400

    Remove unnecessary eslint ignore

--- bitwarden_license/bit-web/src/app/dirt/access-intelligence/activity/activity-cards/password-change-metric.component.ts
+++ bitwarden_license/bit-web/src/app/dirt/access-intelligence/activity/activity-cards/password-change-metric.component.ts
@@ -18,8 +18,6 @@ import { DefaultAdminTaskService } from "../../../../vault/services/default-admi
 import { RenderMode } from "../../models/activity.models";
 import { AccessIntelligenceSecurityTasksService } from "../../shared/security-tasks.service";
 
-// FIXME(https://bitwarden.atlassian.net/browse/CL-764): Migrate to OnPush
-// eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: "dirt-password-change-metric",
